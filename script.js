0e5a9c1cdb472588713e8e0d5b9f57336c7486d5
WalterSit0 <walter.gonzalez@live.com.ar>
2022-01-27 22:25:37 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,78 @@
+
+/* Reglas de encriptación: 
+"e" es convertido para "enter" 
+"i" es convertido para "imes"
+"a" es convertido para "ai"
+"o" es convertido para "ober"
+"u" es convertido para "ufat"
+Solo letras minusculas
+No se permite acentuación de palabras 
+*/
+
+/* Reglas de desencriptación: 
+"enter" es convertido para "e" 
+"imes" es convertido para "i"
+"ai" es convertido para "a"
+"ober" es convertido para "o"
+"ufat" es convertido para "u"
+Solo letras minusculas
+No se permite acentuación de palabras   
+*/
+var botonEncriptar = document.querySelector("#btn-encriptar");
+var botonDesncriptar = document.querySelector("#btn-desencriptar");
+var botonCopiar = document.querySelector("#btn-copy");
+var cajaTexto = document.querySelector("#input-texto");
+var cajaResultado = document.querySelector("#msg");
+
+botonEncriptar.addEventListener("click",function(event){
+    event.preventDefault();
+    cajaResultado.value = Encriptar(cajaTexto.value);
+});
+
+botonDesncriptar.addEventListener("click",function(event){
+    event.preventDefault();
+    cajaResultado.value = Desencriptar(cajaTexto.value);
+});
+
+botonCopiar.addEventListener("click",function(event){
+    event.preventDefault();
+    CopiarAlPortapapeles(cajaResultado.value);
+});
+
+function Encriptar(texto){
+    texto = QuitarAcentuacion(texto)
+    texto = texto.replaceAll("e", "enter");
+    texto = texto.replaceAll("i", "imes");
+    texto = texto.replaceAll("a", "ai");
+    texto = texto.replaceAll("o", "ober");
+    texto = texto.replaceAll("u", "ufat");
+    return texto;
+}
+
+function Desencriptar(texto){
+    texto = QuitarAcentuacion(texto)
+    texto = texto.replaceAll("enter", "e");
+    texto = texto.replaceAll("imes", "i");
+    texto = texto.replaceAll("ai", "a");
+    texto = texto.replaceAll("ober", "o");
+    texto = texto.replaceAll("ufat", "u");
+    return texto;
+}
+
+function QuitarAcentuacion(texto){
+    texto = texto.replaceAll("á", "a");
+    texto = texto.replaceAll("é", "e");
+    texto = texto.replaceAll("í", "i");
+    texto = texto.replaceAll("ó", "o");
+    texto = texto.replaceAll("ú", "u");
+    return texto;
+}
+
+function CopiarAlPortapapeles(texto) {
+    navigator.clipboard.writeText(texto).then(function() {
+        console.log("el texto " + texto + " fue copiado al portapapeles");
+    }, function() {
+        console.log("error al intentar copiar al portapapeles");
+    });
+  }
+  
